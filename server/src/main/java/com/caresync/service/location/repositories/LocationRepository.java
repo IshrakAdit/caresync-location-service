a0265abfc5650e75704c7afddb53860c3a1dfc44
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2025-06-02 20:04:29 +0600

    repository functions: location

--- server/src/main/java/com/caresync/service/location/repositories/LocationRepository.java
+++ server/src/main/java/com/caresync/service/location/repositories/LocationRepository.java
@@ -1,7 +1,15 @@
 package com.caresync.service.location.repositories;
 
 import com.caresync.service.location.entities.Location;
+import com.caresync.service.location.enums.LOCATION_TYPE;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface LocationRepository extends JpaRepository<Location, String> {
+
+    List<Location> findAllByLocationType(LOCATION_TYPE locationType);
+    Optional<Location> findByIdAndLocationType(String id, LOCATION_TYPE locationType);
+
 }
